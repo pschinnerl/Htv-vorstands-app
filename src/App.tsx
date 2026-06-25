351160dba2576e40e3b67fc89a82a3a68c2a1dce
Schinnerling <pschinnerl@t-online.de>
2026-06-25 16:38:50 +0200

    Fix: React Router basename + GitHub Pages SPA 404-Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <UnreadProvider>
           <AppRoutes />
