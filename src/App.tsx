dce6c611d1664685922642e3cade17b89b0f6dd9
Schinnerling <pschinnerl@t-online.de>
2026-06-25 16:48:48 +0200

    Fix: BrowserRouter basename trailing slash entfernen

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <AuthProvider>
         <UnreadProvider>
           <AppRoutes />
